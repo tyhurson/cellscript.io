0a5205fdedbbd93602e72070d59e489a77be36e5
tyhurson <tyler.james.hurson@gmail.com>
2021-05-05 13:52:31 -0700

    Add linter

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,6 +1,6 @@
 import { add } from "@cellscript/util";
 import { foo } from "@cellscript/engine";
 
-export function bar(a: number, b: number) {
+export function bar(a: number, b: number): number {
   return foo(a, b) + add(a, b);
 }
